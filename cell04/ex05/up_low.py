e12c6d64c27dd570fa24350fdecfd4b99da18cbe
phatsanphonna <phatsanphon2004@outlook.com>
2025-09-16 10:44:51 +0700

    fix: cell4-ex5

--- cell04/ex05/up_low.py
+++ cell04/ex05/up_low.py
@@ -1,13 +1,4 @@
 #!/usr/bin/env -S python3
 
 text = input()
-
-output = ""
-
-for char in text:
-    if char.islower():
-        output += char.upper()
-    else:
-        output += char.lower()
-
-print(output)
+print(text.swapcase())
